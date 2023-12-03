fc3882ff0862c65e26979cf5e3356761b76c6aec
Sergey-Astafurov <stupidsociety16@gmail.com>
2023-12-03 21:20:58 +0500

    home

---
+++ homework/hom-3/main.js
@@ -0,0 +1,20 @@
+// const table = document.querySelector('.container')
+// const block = Array.from(document.querySelectorAll('.block'))
+// const playerX = 'X'
+// const player0 = 'O'
+// table.addEventListener('click', (e)=>{
+//     table.classList.toggle('active')
+
+//     const target = e.target
+//     if (target.classList.contains('block')){
+//         if (target.textContent == ''){
+//            return table.classList.contains('active') ? target.textContent = playerX : target.textContent = player0
+//             checkWiner()
+//         }
+//     }
+
+// })
+// function checkWiner(){
+
+
+// }
